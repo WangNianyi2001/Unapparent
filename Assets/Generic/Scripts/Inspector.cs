fff6f32353b00ad9f4e74c6a8da176c20d59f607
Nianyi Wang <Wangnianyi2001@outlook.com>
2022-02-05 10:48:16 +0800

    Show current state in inspector whilst remains readonly

--- Assets/Generic/Scripts/Inspector.cs
+++ Assets/Generic/Scripts/Inspector.cs
@@ -9,6 +9,25 @@ namespace Unapparent {
 		}
 	}
 
+	public class ReadOnlyAttribute : PropertyAttribute { }
+
+	[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+	public class ReadOnlyDrawer : PropertyDrawer {
+		public override float GetPropertyHeight(
+			SerializedProperty property,
+			GUIContent label) {
+			return EditorGUI.GetPropertyHeight(property, label, true);
+		}
+		public override void OnGUI(
+			Rect position,
+			SerializedProperty property,
+			GUIContent label) {
+			GUI.enabled = false;
+			EditorGUI.PropertyField(position, property, label, true);
+			GUI.enabled = true;
+		}
+	}
+
 	public static class IGUI {
 		public static void Indent(Action content) {
 			++EditorGUI.indentLevel;
