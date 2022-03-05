bdeadc76a69b53dcd7433796b4ca4dc713b2116b
Nianyi Wang <Wangnianyi2001@outlook.com>
2022-03-06 07:29:18 +0800

    Update UI layout & store as prefab

--- Assets/Generic/Scripts/Level.cs
+++ Assets/Generic/Scripts/Level.cs
@@ -7,29 +7,29 @@ namespace Unapparent {
 	public class Level : MonoBehaviour {
 		public static Level current => SceneManager.GetActiveScene().GetRootGameObjects()[0]?.GetComponent<Level>();
 
-		public GameObject ui;
+		public GameObject canvas;
 
 		public void ShowMonologue(Monologue monologue) {
-			Transform info = ui.transform.Find("Monologue/Content/Info");
+			Transform info = canvas.transform.Find("Monologue/Content/Info");
 			info.Find("Name").GetComponent<Text>().text = monologue.character.displayName;
 			info.Find("Text").GetComponent<Text>().text = monologue.text;
-			GameObject optionsObj = ui.transform.Find("Monologue/Content/Options").gameObject;
+			GameObject optionsObj = canvas.transform.Find("Monologue/Content/Options").gameObject;
 			foreach(Monologue.Option option in monologue.options) {
 				GameObject optionObj = Monologue.MakeOptionButton(option);
 				optionObj.transform.parent = optionsObj.transform;
 			}
-			ui.gameObject.SetActive(true);
+			canvas.gameObject.SetActive(true);
 		}
 
 		public void CloseMonologue() {
-			GameObject options = ui.transform.Find("Options").gameObject;
+			GameObject options = canvas.transform.Find("Options").gameObject;
 			while(options.transform.childCount > 0)
 				Destroy(options.transform.GetChild(0).gameObject);
 		}
 
 #if UNITY_EDITOR
 		public void Update() {
-			ui = current.gameObject.GetComponentInChildren<Canvas>().gameObject;
+			canvas = current.gameObject.GetComponentInChildren<Canvas>().gameObject;
 		}
 #endif
 	}
