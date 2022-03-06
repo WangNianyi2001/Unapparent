84aa570978adbf3c5ebd15ec9b2b76053e69e053
Nianyi Wang <Wangnianyi2001@outlook.com>
2022-03-06 17:21:05 +0800

    Improvise Logue inspector

--- Assets/Generic/Scripts/CommandSystem/Command/Action/Logue/Logue.cs
+++ Assets/Generic/Scripts/CommandSystem/Command/Action/Logue/Logue.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEditor;
 using UnityEngine;
 
 namespace Unapparent {
@@ -8,18 +9,19 @@ namespace Unapparent {
 		[SerializeField] public List<Monologue> monologues;
 		public Command next = null;
 
-		public void OnEnable() {
+		public override object Execute(Carrier target) {
 			for(int i = 1; i < monologues.Count; ++i)
 				monologues[i - 1].next = monologues[i];
 			monologues.Last().next = null;
+			return monologues.Count == 0 ? null : monologues[0]?.Execute(target);
 		}
-
-		public override object Execute(Carrier target) => monologues.Count == 0 ? null : monologues[0]?.Execute(target);
 
 		public override void Inspect(ArgList<Action> elements) {
 			IGUI.Inline(() => {
 				elements[0]?.Invoke();
 				IGUI.Label("Logue");
+				ShowRefBtn();
+				GUILayout.FlexibleSpace();
 			});
 			IGUI.Indent(() => {
 				for(int i = 0; i < monologues.Count; ++i) {
@@ -27,7 +29,7 @@ namespace Unapparent {
 					Monologue monologue = monologues[j];
 					monologue.Inspect(null, () => {
 						if(IGUI.Button("Remove", IGUI.exWidth)) {
-							if(!IGUI.Confirm("You're removing a monolugue, proceed?"))
+							if(!IGUI.Confirm("Removing a monologue, proceed?"))
 								return;
 							monologues[j]?.Dispose();
 							monologues.RemoveAt(j);
@@ -44,6 +46,12 @@ namespace Unapparent {
 				}
 				elements[1]?.Invoke();
 			});
+		}
+
+		public override void Dispose() {
+			foreach(Monologue monologue in monologues)
+				monologue?.Dispose();
+			base.Dispose();
 		}
 	}
 }
