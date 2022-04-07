1dbaf311c868656b9f99c1df739dc326702c7aa2
Nianyi Wang <Wangnianyi2001@outlook.com>
2022-04-08 04:49:46 +0800

    Appearing command Used to detect if character (mainly the protagonist) is in shape of given identity.

---
+++ Assets/Generic/Scripts/Command/Commands/Expression/Appearing.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+#pragma warning disable 1998
+
+namespace Unapparent {
+	public class Appearing : Expression {
+		public Character subject;
+		public Identity expected;
+
+		public override async Task<object> Execute() {
+			bool res = subject.appearance == expected;
+			return res;
+		}
+	}
+}
